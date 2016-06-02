5e7c14829f6fbad1425edbe882f4d4b7b0c13718
lazydog <yinpan_yp@qq.com>
2016-06-02 13:46:08 +0800

    New changelist

---
+++ app/src/main/java/activity/bz/com/recyclerviewdemo/BActivity.java
@@ -0,0 +1,25 @@
+package activity.bz.com.recyclerviewdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class BActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_a, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
