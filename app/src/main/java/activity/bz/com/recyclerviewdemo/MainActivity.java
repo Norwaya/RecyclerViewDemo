b0cf4e5cdc4597e3be325de073e3f124a0c77e9d
lazydog <yinpan123>
2016-06-03 11:05:24 +0800

    New changelist111

--- app/src/main/java/activity/bz/com/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/activity/bz/com/recyclerviewdemo/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+// line1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
